ea86a7427d9403b16a5c048e23f21c888f233c77
Batoul Allahham <batoolallahham5@gmail.com>
2026-05-09 08:47:46 +0300

    added serializers

--- shop/serializers.py
+++ shop/serializers.py
@@ -60,3 +60,43 @@ class FavoriteSerializer(serializers.ModelSerializer):
     class Meta:
         model = Favorite
         fields = '__all__'
+
+class StoreSerializer(serializers.ModelSerializer):
+    image = serializers.SerializerMethodField()
+
+    class Meta:
+        model = Store
+        fields = ['id', 'name', 'description', 'image', 'location', 'created_at', 'updated_at']
+
+    def get_image(self, obj):
+        if obj.image:
+            request = self.context.get('request')
+            if request is not None:
+                return request.build_absolute_uri(obj.image.url)
+            return obj.image.url
+        return None
+
+
+
+class StoreWithProductsSerializer(StoreSerializer):
+    products = ProductSerializer(many=True, read_only=True)
+
+    class Meta:
+        model = Store
+        fields = ['id', 'name', 'description', 'image', 'location', 'created_at', 'updated_at', 'products']
+
+
+class OrderItemSerializer(serializers.ModelSerializer):
+    product = ProductSerializer(read_only=True)
+
+    class Meta:
+        model = OrderItem
+        fields = ['id', 'product', 'quantity', 'price', 'created_at']
+
+
+class OrderSerializer(serializers.ModelSerializer):
+    items = OrderItemSerializer(many=True, read_only=True)
+
+    class Meta:
+        model = Order
+        fields = ['id', 'user', 'cost', 'state', 'location', 'pay_status', 'created_at', 'updated_at', 'items']
